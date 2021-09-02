5c028b240314e7029b21383a833e12fe6807a7c1
Digambar <deshawaldigambar@gmail.com>
2021-09-03 00:06:08 +0530

    all completes

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output-div");
 
+var url="https://api.funtranslations.com/translate/yoda.json";
+
+function urlConstruct(text){
+    return url+"?text="+text
+}
+
+function errorHandler(error){
+    console.log("error occured"+ error);
+    alert("someting went wrong..! try again after sometimes");
+}
+
 function myFunction(){
-   
-    outputDiv.innerText= "this is trnaslated "+ textInput.value;
+    var inputText=textInput.value;
+    fetch(urlConstruct(inputText))
+    .then(response=> response.json())
+    .then(json=>outputDiv.innerText=json.contents.translated)
+    .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", myFunction);
