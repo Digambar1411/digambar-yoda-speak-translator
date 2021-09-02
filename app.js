0a73bfeb25db4e1f2df97ecde333bdb84f3b9533
Digambar <deshawaldigambar@gmail.com>
2021-09-02 19:42:16 +0530

    taking input and console
    taking input from user and console on click

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
-alert("this is yoda");
-var userName =prompt("tell us your name")
-alert("welcome " +userName+"!!");
+var btnTranslate = document.querySelector("#btn-translate");
+
+var textInput = document.querySelector("#txt-input");
+
+function myFunction(){
+    console.log("Clicked");
+    console.log(textInput.value);
+    
+};
+
+btnTranslate.addEventListener("click", myFunction);
+
+
+
 
